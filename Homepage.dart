46bff918ae0d58ebcb7013e5e2caf80ff99e3278
JoseMariTan <132253808+JoseMariTan@users.noreply.github.com>
2023-11-18 19:14:24 +0800

    added bottom navigation bar

--- Homepage.dart
+++ Homepage.dart
@@ -5,18 +5,21 @@ import 'PopularB.dart';
 import 'TrendingH.dart';
 import 'Settings.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
-  // Sample data for the carousel
-  final List<String> carouselImages = [
-    "https://i.imgur.com/xpiswhK.png",
-    "https://i.imgur.com/abcdefg.png", // Add more image URLs as needed
-  ];
+  int _currentIndex = 0;
 
-  final List<String> carouselTitles = [
-    "Title 1",
-    "Title 2", // Add corresponding titles
+  final List<Widget> _tabs = [
+    HomeTab(),
+    FavoritesTab(),
+    HistoryTab(),
+    BookingTab(),
   ];
 
   @override
@@ -40,22 +43,6 @@ class Homepage extends StatelessWidget {
             _scaffoldKey.currentState?.openDrawer();
           },
         ),
-        actions: [
-          Padding(
-            padding: EdgeInsetsDirectional.fromSTEB(0, 0, 10, 0),
-            child: IconButton(
-              icon: Icon(
-                Icons.search,
-                color: Colors.white,
-                size: 25,
-              ),
-              onPressed: () {
-                // Handle search feature
-                // You can navigate to the search page or implement your logic here
-              },
-            ),
-          ),
-        ],
       ),
       drawer: Drawer(
         child: ListView(
@@ -63,7 +50,7 @@ class Homepage extends StatelessWidget {
           children: [
             DrawerHeader(
               decoration: BoxDecoration(
-                color: Color(0xFF212121), // App Bar Background Color
+                color: Color(0xFF212121),
               ),
               child: Text(
                 'User',
@@ -76,15 +63,13 @@ class Homepage extends StatelessWidget {
             ListTile(
               title: Text('Profile'),
               onTap: () {
-                // Handle profile feature
-                Navigator.pop(context); // Close the drawer
+                Navigator.pop(context);
               },
             ),
             ListTile(
               title: Text('Settings'),
               onTap: () {
-                // Handle settings feature
-                Navigator.pop(context); // Close the drawer
+                Navigator.pop(context);
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => Settings()),
@@ -94,159 +79,230 @@ class Homepage extends StatelessWidget {
             ListTile(
               title: Text('Logout'),
               onTap: () {
-                // Handle logout feature
-                Navigator.pop(context); // Close the drawer
+                Navigator.pop(context);
               },
             ),
           ],
         ),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: CarouselSlider(
-                items: carouselImages.map((item) {
-                  return Builder(
-                    builder: (BuildContext context) {
-                      return Container(
-                        width: MediaQuery.of(context).size.width,
-                        margin: EdgeInsets.symmetric(horizontal: 5.0),
-                        decoration: BoxDecoration(
-                          color: Colors.amber,
-                        ),
-                        child: Image.network(
-                          item,
-                          fit: BoxFit.cover,
-                        ),
-                      );
-                    },
-                  );
-                }).toList(),
-                options: CarouselOptions(
-                  autoPlay: true,
-                  aspectRatio: 2.0,
-                  enlargeCenterPage: true,
-                ),
+      body: _tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color(0xFF333333), // Set the background color here
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.black, // Set the selected item color
+        unselectedItemColor: Colors.grey,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: 'History',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Booking',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HomeTab extends StatelessWidget {
+  final List<String> carouselImages = [
+    "https://i.imgur.com/xpiswhK.png",
+    "https://i.imgur.com/abcdefg.png",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 10),
+            child: CarouselSlider(
+              items: carouselImages.map((item) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: BoxDecoration(
+                        color: Colors.amber,
+                      ),
+                      child: Image.network(
+                        item,
+                        fit: BoxFit.cover,
+                      ),
+                    );
+                  },
+                );
+              }).toList(),
+              options: CarouselOptions(
+                autoPlay: true,
+                aspectRatio: 2.0,
+                enlargeCenterPage: true,
               ),
             ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment:
-                  MainAxisAlignment.center, // Center the children horizontally
-              children: <Widget>[
-                Text(
-                  "Welcome to Grabarber!",
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment:
+                MainAxisAlignment.center, // Center the children horizontally
+            children: <Widget>[
+              Text(
+                "Welcome to Grabarber!",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Roboto', // Updated font
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Container(
+              width: 300.0, // Set width to 300 pixels
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => GetStartedPage(),
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFFFFD700),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  minimumSize:
+                      Size(double.infinity, 60), // Increased button height
+                ),
+                child: Text(
+                  "Get Started",
                   style: TextStyle(
                     color: Colors.black,
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Container(
-                width: 300.0, // Set width to 300 pixels
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => GetStartedPage(),
-                      ),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    primary: Color(0xFFFFD700),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    minimumSize:
-                        Size(double.infinity, 60), // Increased button height
-                  ),
-                  child: Text(
-                    "Get Started",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20, // Increased font size
-                    ),
+                    fontSize: 20, // Increased font size
+                    fontFamily: 'Montserrat', // Updated font
                   ),
                 ),
               ),
             ),
-            SizedBox(height: 20),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Container(
-                width: 300.0,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => PopularB(),
-                      ),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    primary: Color(0xFFFFD700),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Container(
+              width: 300.0,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => PopularB(),
                     ),
-                    minimumSize:
-                        Size(double.infinity, 60), // Increased button height
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFFFFD700),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  child: Text(
-                    "Popular Barbers",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                    ),
+                  minimumSize:
+                      Size(double.infinity, 60), // Increased button height
+                ),
+                child: Text(
+                  "Popular Barbers",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontFamily: 'OpenSans', // Updated font
                   ),
                 ),
               ),
             ),
-            SizedBox(height: 20),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Container(
-                width: 300.0,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => TrendingH(),
-                      ),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    primary: Color(0xFFFFD700),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Container(
+              width: 300.0,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => TrendingH(),
                     ),
-                    minimumSize:
-                        Size(double.infinity, 60), // Increased button height
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFFFFD700),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  child: Text(
-                    "Trending Hairstyles",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                    ),
+                  minimumSize:
+                      Size(double.infinity, 60), // Increased button height
+                ),
+                child: Text(
+                  "Trending Hairstyles",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontFamily: 'Lobster', // Updated font
                   ),
                 ),
               ),
             ),
-            SizedBox(height: 25),
-          ],
-        ),
+          ),
+          SizedBox(height: 25),
+        ],
       ),
+    );
+  }
+}
+
+class FavoritesTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Favorites Tab Content'),
+    );
+  }
+}
+
+class HistoryTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('History Tab Content'),
+    );
+  }
+}
+
+class BookingTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Booking Tab Content'),
     );
   }
 }
