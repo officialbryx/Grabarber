f33c8b44b69473c08549c3504443775a8f3490f7
JoseMariTan <132253808+JoseMariTan@users.noreply.github.com>
2023-11-19 19:28:21 +0800

    included sign in and sign up

--- main.dart
+++ main.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
-import 'Homepage.dart';
+import 'signin.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MaterialApp(
-    home: Homepage(),
+    home: SignInScreen(),
   ));
 }
+
+/*class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<User?> signUpWithEmailAndPassword(
+      String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      User? user = result.user;
+      return user;
+    } catch (error) {
+      print(error.toString());
+      return null;
+    }
+  }
+}*/
